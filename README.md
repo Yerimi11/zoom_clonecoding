366b51bca63a8c8e1337d3fb87192a9c07e840c9
yerim <sontt4266@naver.com>
2022-02-13 01:55:46 +0900

    2.4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # zoom_clonecoding
+
+Zoom Clone using NodeJS, WebRTC and Websockets.
 니꼬~
---
+++ src/server.js
@@ -0,0 +1,53 @@
+import http from "http";
+import SocketIO from "socket.io";
+import express from "express";
+
+const app = express();
+
+// express가 하는 일 (2)
+// 1. views를 설정해준다
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+// 2. render 해준다
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app); 
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+    });
+});
+
+
+/* const wss = new WebSocket.Server({ server });
+const sockets = [];
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+    console.log("Connected to Browser 💐");
+    socket.on("close", () => console.log("Disconnected from the Browser 🥀"));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => 
+                    aSocket.send(`${socket.nickname}: ${message.payload}`)
+                );
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+    });
+});
+ */
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
